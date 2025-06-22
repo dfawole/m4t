c05df3294ec7f4f52483bbe5eb2684029ae068bc
Ola Fawole <dfawole66@gmail.com>
2025-06-22 22:03:31 +0100

    update esbuild.config.js and package.json

---
+++ esbuild.config.js
@@ -0,0 +1,11 @@
+// esbuild.config.js
+import { build } from 'esbuild';
+
+build({
+	entryPoints: ['server/index.ts'],
+	bundle: true,
+	platform: 'node',
+	format: 'esm',
+	outdir: 'dist',
+	external: ['@babel/preset-typescript/package.json', 'lightningcss', 'lightningcss/*'],
+}).catch(() => process.exit(1));
